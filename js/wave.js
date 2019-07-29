1b7f80ed40860be2165c96a250daf3c511c7de2d
plutoshe <plutoshe@gmail.com>
2019-07-29 21:27:12 +0800

    add neighboor structure setting

--- js/wave.js
+++ js/wave.js
@@ -6,11 +6,39 @@ class Wave
         this.m_relations = [];
         this.m_result = [];
         this.m_size = 0;
+        this.entropies = new HEAP();
+    }
+
+
+    // neighboor constrain structure:
+    // {
+    //    from: curentBlockID
+    //    to: nextBlockID
+    //    dir: [direction which constrain exist]
+    LoadConstrainsByNeighboors(neighboors) 
+    {
+        for (var i = 0; i < neighboors.length; i++)
+        {
+            for (var j = 0; j < neighboors[i].dirs.length; j++)
+            {
+                this.status[neighboors[i].dirs[j]][neighboors[i].from][neighboors[i].to] = true;
+            }
+        }
     }
 
     Load() 
     {
+        this.status = new bool[this.dirNum][this.patternNum][this.patternNum];
+    }
 
+    Select() 
+    {
+        var entropyArgMin = this.entropies.GetMin();
+        this.m_slots[entropyArgMin].GeneratingPattern();
+    }
+
+    Influence() {
+        
     }
 
     Generating()
@@ -21,7 +49,7 @@ class Wave
             if (currentGeneratingItem.Select()) 
             {
                 this.m_result.push(currentGeneratingItem);
-                wave.Influence(currentGeneratingItem);
+                wave.Influence();
             } 
             else 
             {
@@ -37,7 +65,7 @@ class Wave
     }
 
     Pop() {
-        this.
+
     }
 }
 
